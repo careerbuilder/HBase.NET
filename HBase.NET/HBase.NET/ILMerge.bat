05f62b4b4832fb02e44cb0a53dd74dbadb10524d
thomas_rega_cp <SND\thomas_rega_cp>
2014-05-01 00:04:41 +0000

    Using the %1 var

--- HBase.NET/HBase.NET/ILMerge.bat
+++ HBase.NET/HBase.NET/ILMerge.bat
@@ -1,4 +1,4 @@
 CD %~dp0
 IF NOT EXIST ..\bin MKDIR ..\bin
-ILMerge.exe bin\Debug\HBase.NET.dll ..\dll\Thrift.dll dll\Castle.Core.dll dll\log4net.dll /targetplatform:v4 /ndebug /internalize /out:..\bin\HBase.NET.dll
+ILMerge.exe %1 ..\dll\Thrift.dll dll\Castle.Core.dll dll\log4net.dll /targetplatform:v4 /ndebug /internalize /out:..\bin\HBase.NET.dll
 COPY "..\Open Source Licenses.txt" ..\bin

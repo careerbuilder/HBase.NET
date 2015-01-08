d4d8228f3e4398a6e58eab1cd2644f3f79fe3b0d
thomas_rega_cp <SND\thomas_rega_cp>
2015-01-08 12:38:36 +0000

    Updating dependencies.
    Thrift 0.9.2 -> Thrift 0.9.1
    Castle 3.0.0 -> Castle 3.3.3
    Log4net 1.2.11 -> Log4net 1.2.13

--- HBase.NET/HBase.NET/autogenerate.bat
+++ HBase.NET/HBase.NET/autogenerate.bat
@@ -12,4 +12,4 @@
 :: See the License for the specific language governing permissions and
 :: limitations under the License.
 
-thrift-0.9.1.exe -o . --gen csharp Hbase.thrift
+thrift-0.9.2.exe -o . --gen csharp Hbase.thrift
